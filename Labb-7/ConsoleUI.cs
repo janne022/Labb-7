eaa7e483709bceb5b0704a9af0b1497870175924
Johannes Flodin <jannis020609@gmail.com>
2025-09-15 22:05:19 +0200

    added dictonary to invoke actions based on option chosen by user in menu. Main refrence point: https://csharp.progdocs.se/grundlaggande/delegates

--- Labb-7/ConsoleUI.cs
+++ Labb-7/ConsoleUI.cs
@@ -9,21 +9,39 @@ namespace Labb_7
     internal class ConsoleUI
     {
         private Menu menu;
+        readonly Dictionary<MenuOptions, Action> optionMenu = new Dictionary<MenuOptions, Action>()
+        {
+            {MenuOptions.StartQuiz, DisplayQuestions},
+            {MenuOptions.ManageQuiz, ReadInput},
+            {MenuOptions.Exit, Exit}
+
+        };
 
         public ConsoleUI()
         {
             this.menu = new Menu();
         }
-
+        // Calls ShowOptions to get the options that the user showed, then invokes the method using a simple dictonary lookup. Dictonary may return error if MenuOptions is expanded without dictonary expansion.
         public void ShowMenu()
         {
-            menu.ShowOptions();
+            MenuOptions chosenOption = menu.ShowOptions();
+            Console.Clear();
+            Action action = optionMenu[chosenOption];
+            action.Invoke();
         }
-        void ReadInput()
+        private static void ReadInput()
         {
-        
+            Console.WriteLine("Edit Quiz");
         }
 
-        void DisplayQuestions() { }
+        private static void DisplayQuestions()
+        {
+            Console.WriteLine("Quiz started");
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("Exiting");
+        }
     }
 }
